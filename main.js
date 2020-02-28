51a884acf1e57ac1116d85a4aab7257470a89ecf
Marcus <marcus@apwwgroup.com>
2020-02-28 09:40:21 +0000

    Comlpeted general project setup

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * File: \main.js
+ * Project: alremote
+ * Created Date: 28/02/2020 @ 09:38
+ * Author: Marcus Setchell
+ * -----
+ * Last Modified: 28/02/2020 @ 09:39
+ * Modified By: Marcus Setchell
+ * -----
+ */
+
+
+console.log('Project Setup!');
